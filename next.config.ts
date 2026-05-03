fc0abbe5dfb812e13a7390cbd7f8292d509228ff
sktiwari2907 <sktiwari2907@gmail.com>
2026-05-03 20:36:21 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    turbopack: {
+      resolveAlias: {
+        canvas: './empty-module.js', // Or simply false if supported
+      },
+    },
+  },
   webpack: (config) => {
     config.resolve.alias.canvas = false;
     return config;
-  }
+  },
 };
 
 export default nextConfig;
